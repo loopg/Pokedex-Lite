441d955ba0f4718d79aface0f0ca20c11fd6eeb4
Sahil Pandey <vats2003sahil@gmail.com>
2024-12-28 11:43:04 +0530

    changes eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,13 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+module.exports = {
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "next/next/no-img-element": "off",
+    "@typescript-eslint/no-unused-vars": "warn"
+  }
+};
+
+
 export default eslintConfig;
